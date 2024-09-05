21818b559627912f9c84fb3c019f590414e74316
Leonardo Kazuhiro Tomisaki <168482917+leonardotomisaki@users.noreply.github.com>
2024-09-04 23:43:28 -0300

    aulas e ex

---
+++ aulas/aula16.py
@@ -0,0 +1,16 @@
+#lanche = ('Pizza', 'Bolo', 'Panqueca', 'Suco')
+
+#for comida in lanche:
+    #print(f'Eu vou comer {comida}')
+
+#for cont in range(0, len(lanche)):
+    #print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+
+#for pos, comida in enumerate(lanche):
+    #print(f'Eu vou comer {comida} na posição {pos}')
+
+a = (6,5,2,4)
+b = (1,8,3,9)
+c = a + b
+
+print(len(c))
